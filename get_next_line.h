efb2c9e39c070cd4e95bee48f07942965815ec44
Rostyslav Kobeliev <106913207+kobrosss@users.noreply.github.com>
2024-07-28 17:24:26 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkobeliev <rkobeliev@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 16:52:08 by rkobeliev         #+#    #+#             */
+/*   Updated: 2024/04/18 22:22:38 by rkobeliev        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*get_next_line(int fd);
+char	*get_line(char *left_str);
+char	*formatnewline(char *rude_str);
+char	*readnewline(int fd, char *rude_str);
+
+#endif
